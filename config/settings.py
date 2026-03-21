753a2f75c20b389001a4a565315f3172ec5ad35a
Gleb-Pinchuk <ghliebp@bk.ru>
2026-03-21 20:33:12 +0300

    fix: я

--- config/settings.py
+++ config/settings.py
@@ -91,11 +91,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
+STATICFILES_DIRS = [BASE_DIR / 'frontend']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend'),
-]
 
 # Media files
 MEDIA_URL = '/media/'
--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('app/', TemplateView.as_view(template_name='index.html'), name='app'),
 ]
 
-# ✅ ПРАВИЛЬНОЕ подключение статики и медиа (только в DEBUG)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
