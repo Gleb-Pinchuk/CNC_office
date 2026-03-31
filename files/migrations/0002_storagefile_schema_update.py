ccd8805bd9378b347dce9a0ba63b65cfde5e96d4
Gleb-Pinchuk <ghliebp@bk.ru>
2026-03-31 21:47:57 +0300

    fix: handle foreign key columns in files migration

--- files/migrations/0002_storagefile_schema_update.py
+++ files/migrations/0002_storagefile_schema_update.py
@@ -31,7 +31,8 @@ def _index_exists(schema_editor, table_name, index_name):
 class AddFieldIfMissing(migrations.AddField):
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         model = to_state.apps.get_model(app_label, self.model_name)
-        column_name = self.field.db_column or self.name
+        _, column_name = self.field.get_attname_column()
+        column_name = column_name or self.field.db_column or self.name
         if _column_exists(schema_editor, model._meta.db_table, column_name):
             return
         super().database_forwards(app_label, schema_editor, from_state, to_state)
